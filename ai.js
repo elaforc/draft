0c557e95c9bed66a3ac486cf5dbc795fe65cca56
Eric LaForce <elaforc@gmail.com>
2020-12-05 06:37:28 -0500

    Added alpha beta pruning

--- ai.js
+++ ai.js
@@ -14,7 +14,7 @@ class AI {
     for (let i = 0; i < legalMoves.length; i++) {
       let possibleMove = legalMoves[i];
       let child = this.board.performMove(possibleMove);
-      let childValue = this.minimax(child, this.depth, this.color);
+      let childValue = this.alphabeta(child, this.depth, this.color, -Infinity, Infinity);
       if (childValue > currentValue) {
         currentIndex = i;
         currentValue = childValue;
@@ -22,6 +22,44 @@ class AI {
     }
 
     return legalMoves[currentIndex];
+  }
+
+  alphabeta(board, depth, maximizingPlayer, alpha, beta) {
+    if (depth === 0 || (board.getLegalMoves('r').length === 0 && board.getLegalMoves('b').length === 0)) {
+      return board.heuristic();
+    }
+
+    if (maximizingPlayer === this.color) {
+      let value = -Infinity;
+      let legalMoves = board.getLegalMoves(this.color);
+      for (let i = 0; i < legalMoves.length; i++) {
+        let possibleMove = legalMoves[i];
+        let child = board.performMove(possibleMove);
+        value = Math.max(value, this.alphabeta(child, depth - 1, false, alpha, beta))
+        alpha = Math.max(alpha, value);
+        if (alpha >= beta) {
+          break;
+        }
+      }
+
+      return value;
+    }
+
+    else {
+      let value = Infinity;
+      let legalMoves = board.getLegalMoves(this.adversary);
+      for (let i = 0; i < legalMoves.length; i++) {
+        let possibleMove = legalMoves[i];
+        let child = board.performMove(possibleMove);
+        value = Math.min(value, this.alphabeta(child, depth - 1, true, alpha, beta))
+        beta = Math.min(beta, value);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+
+      return value;
+    }
   }
 
   minimax(board, depth, maximizingPlayer) {
--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const constants = {
   BOARD_LENGTH: 8,
-  DEPTH: 3
+  DEPTH: 4
 }
 
 module.exports.constants = constants;
